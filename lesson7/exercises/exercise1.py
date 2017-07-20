342ffaf2c26909eee71f89e4101da1bb5b97a959
Jennifer Basalone <jbasalone@jbasalone-mbp.corp.dropbox.com>
2017-07-19 17:55:10 -0700

    initial commit lesson 7

---
+++ lesson7/exercises/exercise1.py
@@ -0,0 +1,22 @@
+# Build a  list and dictionary of Cisco Buildings that have Snacks and an Open Plan and a
+# dictionary that has parking
+
+# List of Buildings
+# Building Name, Snacks?, Open Floor Plan?
+
+buildings = [
+         ('i',  False, True),
+         ('32', True,  True),
+         ('18', True,  True),
+         ('m',  False, False),
+         ]
+
+# Building i, has  parking 
+# Building 32, does not have parking
+# Building 18, does not have parking
+# Building m, has parking 
+
+parking = {}
+
+# Start Code
+# ----------
---
+++ lesson7/exercises/homework1.py
@@ -0,0 +1,20 @@
+'''
+create a dictionary of all the names and ages of the Bobs Burgers Characters
+(e.x. characters = {‘Name”: “Bob”, “Age”: 44} ) 
+
+create a function to see if your dictionary has BB show character named ‘Leon’
+by using your_dict.has_keys(‘Leon’) method and print if the name is there or
+not. 
+
+print all the names of the dictionary using your_dict.keys()
+create a variable that has a list of all your values using your_dict.values()
+
+Sort the dictionary and print it
+
+print the length of the dictionary using len()
+
+add your own flare and fun from what you have learned. 
+'''
+
+
+
